039ce27fca6afa052803527fcb1fcda46c325ab9
briantam23 <briantam23@gmail.com>
2019-06-22 10:10:45 -0400

    First commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+// Static file-serving middleware
+app.use('/public', express.static(path.join(__dirname, '../public')));
+
+// Sends our index.html (the "single page" of our SPA)
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+
+
+// Error catching endware
+app.use((err, req, res, next) => {
+    
+    // send back error status
+    res.status(err.status || 500).end();
+})
+
+
+module.exports = app;
