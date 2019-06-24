c7c08f5acb440209c1df704338d6fd1532cb27ec
briantam23 <briantam23@gmail.com>
2019-06-24 08:33:54 -0400

    Added LESS to transactions component

--- src/components/transactions/Transactions.js
+++ src/components/transactions/Transactions.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import style from './transactions.less';
-import { ListGroup, ListGroupItem, Button, Badge } from 'reactstrap';
+import { ListGroup, ListGroupItem, Button, Badge, Jumbotron } from 'reactstrap';
 import { findStockNameById, findFinishedTransactions } from '../../util';
 
 
@@ -10,37 +10,54 @@ const Transactions = ({ transactions, stocks, auth }) => (
         <h2>Transactions</h2>
         <hr/>
     {
-        transactions.map(transaction => (
-            <ListGroup className={ style.listGroup } key={ transaction.id }>
-                <h5>
-                    <strong>Transaction ID: </strong>
-                    <br/>{ transaction.id }
-                </h5>
-            {
-                transaction.lineItems.map(lineItem => (
-                    <ListGroupItem key={ lineItem.id }>
-                        <h5>
-                            <strong>Stock Symbol: </strong>
-                            { findStockNameById(stocks, lineItem.stockId) }
-                        </h5>
-                        <h5>
-                            <strong>Quantity: </strong>
-                            { lineItem.quantity }
-                        </h5>
-                    </ListGroupItem>
-                ))
-            }
-            {
-                transaction.totalCost ? (
-                    <h4 className={ style.transactionTotal }>
-                        <strong>Total: </strong> 
-                        ${ transaction.totalCost }
-                    </h4> 
-                ): null
-            }
-                <hr/>
-            </ListGroup>
-        ))
+        transactions[0] ? (
+            transactions.map(transaction => (
+                <ListGroup className={ style.listGroup } key={ transaction.id }>
+                    <h5>
+                        <div className={ style.listGroupHeaders }>
+                            <div className={ style.listGroupHeader }>
+                                <strong>Transaction ID: </strong>
+                                <br/>
+                                { transaction.id }
+                                <br/>
+                            </div>
+                            <div className={ style.listGroupHeader }>
+                                <strong>Ordered on:</strong> 
+                                <br/>
+                                { transaction.updatedAt.slice(0, 10) } <strong>at</strong> { transaction.updatedAt.slice(11, 16) }
+                            </div>
+                        </div>
+                    </h5>
+                {
+                    transaction.lineItems.map(lineItem => (
+                        <ListGroupItem color='info' key={ lineItem.id }>
+                            <h5>
+                                <strong>Stock Symbol: </strong>
+                                { findStockNameById(stocks, lineItem.stockId) }
+                                <br/>
+
+                                <strong>Quantity: </strong>
+                                { lineItem.quantity }
+                            </h5>
+                        </ListGroupItem>
+                    ))
+                }
+                {
+                    transaction.totalCost ? (
+                        <h4 className={ style.transactionTotal }>
+                            <strong>Total: </strong> 
+                            ${ transaction.totalCost }
+                        </h4> 
+                    ): null
+                }
+                    <hr/>
+                </ListGroup>
+            ))
+        ) : (
+            <Jumbotron className={ style.noTransactions }>
+                <h3>No Transactions!</h3>
+            </Jumbotron>
+        )
     }
     </Fragment>
 )
